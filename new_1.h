051c337e9d31299bdbf225c1e5e8e8d2c872d9d9
SSAFR <160252237+SSAFR@users.noreply.github.com>
2024-02-16 17:25:20 +0200

    HTML code

---
+++ new_1.h
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>\
+<head>\
+  <title>IOT PROJECT</title>\
+  <meta http-equiv=\"refresh\" \content=\"5; ur1=http://192.168.1.10\"/>
+  <meta charset="utf-8">
+  <h2>  Titorial <script src="50"></script></h2>
+   <a href="?relay=on" class="btn btn-primary" role="button">Relayon</a>
+   <a href="?relay=off" class="btn btn-danger" role="button">Relayoff</a>
+   <a href="?togle=on" class="btn btn-default" role="button">TOGGLE</a>
+   <div class="alert alert-success">
+    <strong>OKSIR!</strong> Temperatur_e rate is OK.
+  </div>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h2>Temperatur_e</h2>
+    <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar"
+  aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:%f%%">
+    %f%%
+    </div>
+  </body>
+</html>
